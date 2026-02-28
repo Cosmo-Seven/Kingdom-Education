7e8af44ce1f9eb28cbd210eae8ffad12225860b9
Daniel <codedbydaniel@gmail.com>
2026-02-28 14:35:20 +0630

    donation model change

--- models/donation_models.py
+++ models/donation_models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from models.base_models import BaseModel
 from enums.donation import StatusEnum
+
+
 class DonationModel(BaseModel):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     phone = models.CharField(max_length=150)
     amount = models.BigIntegerField(default=0)
     payment = models.CharField(max_length=150)
-    program = models.ManyToManyField("core.ProgramModel",related_name="donations")
-    status = models.CharField(max_length=100,choices=StatusEnum)
+    program = models.ManyToManyField("core.ProgramModel", related_name="donations")
+    status = models.CharField(
+        max_length=20, choices=StatusEnum.choices, default=StatusEnum.YES
+    )
 
     def __str__(self):
         return self.name
+
     class Meta:
         app_label = "core"
         db_table = "donation"
-
